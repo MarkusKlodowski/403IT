a1368021c7a6d7109687ed3c419236ddb19da0c7
MarkusKlodowski <klodowskim@coventry.ac.uk>
2023-09-19 08:56:10 +0200

    first commit

---
+++ In CLass/rubixCuberSolver.py
@@ -0,0 +1,7 @@
+#generate a matrix that represents a rubix cube
+#each face is represented by a 3x3 matrix
+#each face is represented by a color
+
+cube = [[[0 for x in range(3)] for y in range(3)] for z in range(6)]
+
+print(cube)
