dcb0362c691143162ef90a078d9b1913e9c4af3b
MarkusKlodowski <klodowskim@coventry.ac.uk>
2023-09-26 10:53:48 +0200

    updated file name

--- In CLass/rubixCuberSolver.py
+++ In CLass/rubixCuberSolver.py
@@ -1,7 +1,6 @@
-#generate a matrix that represents a rubix cube
-#each face is represented by a 3x3 matrix
-#each face is represented by a color
+import random
 
-cube = [[[0 for x in range(3)] for y in range(3)] for z in range(6)]
-
-print(cube)
+#random number 1-3
+options = ['docker', 'kubernetes', 'jenkins']
+choice = random.choice(options)
+print(choice)
